5f630b183fa9a331c5928c7f047fd9a3be15b206
chinhtran07 <chinh.t1807@gmail.com>
2024-05-17 20:33:37 +0700

    update ui admin

--- studentManagement/index.py
+++ studentManagement/index.py
@@ -1,8 +1,10 @@
+from random import random
+
+from flask import redirect, request, render_template, session, jsonify
+from flask_login import login_user, current_user, logout_user
 import math
 import pdb
 
-from flask import render_template, request, redirect, session
-from flask_login import login_user, current_user, logout_user, login_required
 
 from studentManagement import app, dao, login
 from studentManagement.decorators import logged_in
@@ -30,21 +32,9 @@ def login_my_user():
             url = "/" + role.lower()
             return redirect(next if next else url)
         else:
-            err_msg = 'Username hoặc password không đúng!'
+            err_msg = 'Tài khoản hoặc mật khẩu không đúng!'
 
     return render_template('login.html', err_msg=err_msg)
-
-
-@app.route("/admin-login", methods=['post'])
-def process_admin_login():
-    err_msg = ''
-    username = request.form.get('username')
-    password = request.form.get('password')
-    u = dao.auth_user(username=username, password=password, role=UserRole.ADMIN)
-    if u:
-        login_user(user=u)
-
-    return redirect('/admin')
 
 
 @app.route('/logout', methods=['get'])
@@ -60,7 +50,36 @@ def load_user(user_id):
 
 @app.route('/employee')
 def employee():
-    return render_template("index.html")
+    total_student = dao.get_student()
+    return render_template("employee/employee.html", total_student=total_student)
+
+
+@app.route('/employee/adjust_regulations')
+def adjust_regulations():
+    return render_template('employee/adjust_regulations.html')
+
+
+@app.route('/employee/add_student', methods=['GET', 'POST'])
+def add_student():
+    if request.method == 'POST':
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        gender = request.form['gender']
+        dob = request.form['dob']
+        address = request.form['address']
+        phone_number = request.form['phone_number']
+        avatar = request.form['avatar']
+
+        dao.add_student_info(first_name, last_name, gender, dob, address, phone_number, avatar)
+        student_info = dao.get_student_info(phone_number)
+        return render_template('employee/student_info.html', student_info=student_info)
+    else:
+        return render_template('employee/add_student.html')
+
+
+@app.route('/employee/subject_managements')
+def get_subject():
+    return render_template('employee/subject_managements.html', subjects=dao.get_subject())
 
 
 @app.route('/teacher')
@@ -72,18 +91,20 @@ def teacher():
     classes = dao.load_class(grade=grade, page=page, class_name=class_name)
     total_class = dao.count_class()
 
-    teacher = dao.get_teacher_id(current_user.id)
-    for t in teacher:
-        id = t.id
-        teach_class = dao.get_teach_class(teacher_id=id)
+    teach_class = dao.get_teach_class(teacher_id=dao.get_teacher_id(current_user.id).id)
+    teach_subject = dao.get_teach_subject(teacher_id=dao.get_teacher_id(current_user.id).id)
 
-    return render_template('teacher.html', classes=classes,
-                           pages=math.ceil(total_class / 4), teach_class=teach_class)
+    return render_template('teacher/teacher.html', classes=classes,
+                           pages=math.ceil(total_class / 4), teach_class=teach_class, teach_subject=teach_subject)
 
 
-@app.route('/teacher/scoreManagement')
+@app.route('/teacher/score_management')
 def score_management():
-    return render_template('scoreManagement.html')
+    class_name = request.args.get('class_name')
+
+    teach_class = dao.get_teach_class(teacher_id=dao.get_teacher_id(current_user.id).id, class_name=class_name)
+
+    return render_template('teacher/score_management.html', teach_class=teach_class)
 
 
 @app.route('/teacher/list_student')
@@ -92,7 +113,109 @@ def list_student():
 
     students = dao.get_list_student(class_id=class_id)
 
-    return render_template('listStudent.html', students=students)
+    return render_template('teacher/list_student.html', students=students)
+
+
+@app.route('/teacher/score_input')
+def score_input():
+    class_obj = dao.load_class(class_id=request.args.get('class_id'))
+    subject = dao.get_subject(subject_id=request.args.get('subject_id'))
+    period = dao.get_period(period_id=request.args.get('period'))
+    students = dao.get_list_student(class_id=class_obj.id)
+    scores = dao.get_score(period_id=period.id, class_id=class_obj.id, subject_id=subject.id)
+
+    return render_template(template_name_or_list='teacher/score_input.html', class_obj=class_obj,
+                           subject=subject, period=period, students=students, scores=scores)
+
+
+@app.route('/teacher/score_table')
+def score_table():
+    class_obj = dao.load_class(class_id=request.args.get('class_id'))
+    subject = dao.get_subject(subject_id=request.args.get('subject_id'))
+    period = dao.get_period(period_id=request.args.get('period'))
+    students = dao.get_list_student(class_id=class_obj.id)
+    scores = dao.get_score(period_id=period.id, class_id=class_obj.id, subject_id=subject.id)
+    total_score = dao.count_scores(subject_id=subject.id, class_id=class_obj.id, period_id=period.id)
+    total_score_input = dao.count_total(class_id=class_obj.id) * (
+            subject.exam_15mins + subject.exam_45mins + 1)
+    list_avr = []
+    if total_score >= total_score_input:
+        for student in students:
+            list_avr.append(
+                dao.get_average_scores(student_id=student.id, subject_id=subject.id, period_id=period.id,
+                                       class_id=class_obj.id))
+    return render_template(template_name_or_list='teacher/score_table.html', class_obj=class_obj,
+                           subject=subject, period=period, students=students, scores=scores, total_score=total_score,
+                           total_score_input=total_score_input, list_avr=list_avr)
+
+
+@app.route('/api/teacher/scores', methods=['post'])
+def add_to_scores():
+    scores = session.get('scores')
+    if not scores:  # nếu ko có cái giỏ thì tạo cái giỏ rỗng
+        scores = {}
+
+    # request.json.get('student_id')
+    id = str(random.randint(0, 100))
+
+    sd_id = request.json.get('student_id')
+    sd_scr = request.json.get("score")
+    scr_type = request.json.get("type")
+
+    class_id = request.json.get('class_id')
+    subject_id = request.json.get('subject_id')
+    period_id = request.json.get('period_id')
+
+    subject = dao.get_subject(subject_id=subject_id)
+
+    counter = dao.count_scores(student_id=sd_id, class_id=class_id, subject_id=subject_id, score_type=scr_type,
+                               period_id=period_id)
+    counter_sess = dao.count_scores_in_session(scores, type=scr_type, student_id=sd_id)
+
+    if scr_type == 'ScoreType.EXAM_15MINS':
+
+        if counter + counter_sess >= subject.exam_15mins:
+            return jsonify({'id': 1, 'message': "Đã đủ điểm cột 15 phút", 'status': 500})
+
+    elif scr_type == 'ScoreType.EXAM_45MINS':
+
+        if counter + counter_sess >= subject.exam_45mins:
+            return jsonify({'id': 2, 'message': "Đã đủ điểm cột 45 phút", 'status': 500})
+
+    elif scr_type == 'ScoreType.EXAM_FINAL':
+
+        if counter + counter_sess >= 1:
+            return jsonify({'id': 3, 'message': "Đã đủ điểm cột điểm thi", 'status': 500})
+
+    scores[id] = {
+        "student_id": sd_id,
+        "score": sd_scr,
+        "type": scr_type,
+    }
+    session['scores'] = scores
+
+    print("session['scores'] ")
+    print(session['scores'])
+    return jsonify({'id': 4, 'message': "Thêm thành công", 'status': 200})
+
+
+@app.route('/api/teacher/save_scores', methods=['post'])
+def save_scores():
+    subject_id = request.args.get('subject_id')
+    period_id = request.args.get('period_id')
+    scores = session.get('scores')
+    if scores:
+        try:
+            dao.create_score(scores=scores, subject_id=subject_id, period_id=period_id)
+        except Exception as ex:
+            print(ex)
+            del session['scores']
+            return jsonify({'status': 500})
+        else:
+            del session['scores']
+            return jsonify({'status': 200})
+
+    return jsonify({'status': 500})
 
 
 if __name__ == '__main__':
