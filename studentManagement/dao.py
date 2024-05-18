0258ab4ffe477be5159791454e0c8e67c3bb75a9
Kiet Nguyen <kiethotboy0979@gmail.com>
2024-05-18 20:23:19 +0700

    code hot fix

--- studentManagement/dao.py
+++ studentManagement/dao.py
@@ -5,6 +5,8 @@ from sqlalchemy import desc
 from studentManagement import db
 from studentManagement.models import *
 
+import datetime
+
 
 def get_most_recent_period():
     return Period.query.order_by(desc(Period.id)).first()
@@ -73,6 +75,105 @@ def get_student():
 def get_student_info(phone_number):
     return Information.query.filter_by(phone_number=phone_number).first()
 
+
+def get_all_student_info():
+    return [{'id': student.id, 'name': student.last_name + ' ' + student.first_name,
+             'gender': student.gender.name, 'admission_date': student.admission_date if student.admission_date else '',
+             'dob': student.dob if student.dob else ''} for student in Student.query.all()]
+
+
+def get_student_by_id(id):
+    return Student.query.get(id)
+
+
+def create_or_update_student(id, first_name, last_name, gender, admission_date, dob, address, email, phone_number,
+                             is_active):
+    #try:
+        if id:
+            (Student.query.filter_by(id=id).update({
+                'first_name': first_name,
+                'last_name': last_name,
+                'gender': gender,
+                'admission_date': admission_date,
+                'dob': dob,
+                'address': address,
+                'email': email,
+                'phone_number': phone_number,
+                'is_active': is_active
+            }))
+        else:
+            db.session.add(Student(first_name=first_name, last_name=last_name, gender=gender,
+                                   admission_date=admission_date, dob=dob, address=address,
+                                   email=email, phone_number=phone_number, is_active=is_active))
+
+        db.session.commit()
+
+    #   student_age = datetime.now().year - dob.year
+    #     if 15 <= student_age <= 20:
+    #         db.session.commit()
+    #     else:
+    #         db.session.rollback()
+    #
+    # except Exception as e:
+    #     db.session.rollback()
+    #     return "Lỗi" + str(e)
+    # return None
+
+
+def delete_student(id):
+    Student.query.filter_by(id=id).delete()
+    db.session.commit()
+
+
 def get_subject():
     all_subject = Subject.query.all()
     return all_subject
+
+
+def get_all_classroom_info():
+    return [{'id': c.id, 'name': c.name,
+             'student_count': db.session.query(func.count(Student.id))
+             .join(StudentClass)
+             .filter(StudentClass.class_id == c.id).scalar()}
+            for c in Class.query.all()]
+
+
+def get_classroom_by_id(id):
+    return Class.query.get(id)
+
+
+# def delete_classroom(id):
+#     Class.query.filter_by(id=id).delete()
+#     db.session.commit()
+
+def delete_classroom(id):
+    try:
+        # Xóa các tham chiếu từ bảng Teach và FormTeacher
+        Teach.query.filter_by(class_id=id).delete()
+        FormTeacher.query.filter_by(class_id=id).delete()
+
+        # Xóa lớp
+        class_to_delete = Class.query.get(id)
+        db.session.delete(class_to_delete)
+        db.session.commit()
+        print("Classroom deleted successfully.")
+    except Exception as e:
+        db.session.rollback()
+        print("Error:", e)
+
+
+def create_or_update_classroom(id, name, list_student_id):
+    if id:
+        classroom = Class.query.get(id)
+    else:
+        classroom = Class()
+
+    student_class_list = [StudentClass(student_id=student_id,
+                                       class_id=classroom.id)
+                          for student_id in list_student_id]
+    classroom.student_class = student_class_list
+
+    classroom.name = name
+    if not id:
+        db.session.add(classroom)
+    db.session.commit()
--- studentManagement/index.py
+++ studentManagement/index.py
@@ -1,9 +1,10 @@
 from flask import render_template, request, redirect
 from flask_login import login_user, current_user, logout_user
 
-
 from studentManagement import app, dao, login
 from studentManagement.decorators import logged_in
+from studentManagement.forms import StudentForm, ClassroomForm
+from studentManagement.models import Student, Class
 
 
 @app.route('/')
@@ -68,28 +69,84 @@ def adjust_regulations():
     return render_template('employee/adjust_regulations.html')
 
 
-@app.route('/employee/add_student', methods=['GET', 'POST'])
-def add_student():
-    if request.method == 'POST':
-        first_name = request.form['first_name']
-        last_name = request.form['last_name']
-        gender = request.form['gender']
-        dob = request.form['dob']
-        address = request.form['address']
-        phone_number = request.form['phone_number']
-        avatar = request.form['avatar']
-
-        dao.add_student_info(first_name, last_name, gender, dob, address, phone_number, avatar)
-        student_info = dao.get_student_info(phone_number)
-        return render_template('employee/student_info.html', student_info=student_info)
-    else:
-        return render_template('employee/add_student.html')
+@app.route('/employee/students/', methods=['GET'])
+def student_list():
+    students = dao.get_all_student_info()
+    return render_template('/employee/students.html', students=students)
 
 
-@app.route('/employee/subject_managements')
-def get_subject():
-    return render_template('employee/subject_managements.html', subjects=dao.get_subject())
+@app.route('/employee/students/', methods=['POST'])
+def add_or_update_student():
+    form = StudentForm()
+    if form.validate_on_submit():
+        dao.create_or_update_student(id=form.id.data, first_name=form.first_name.data, last_name=form.last_name.data,
+                                     gender=form.gender.data, admission_date=form.admission_date.data,
+                                     dob=form.dob.data,
+                                     address=form.address.data, email=form.email.data,
+                                     phone_number=form.phone_number.data,
+                                     is_active=form.is_active.data)
 
+        return redirect('/employee/students')
+
+
+@app.route('/employee/students/<int:id>', methods=['GET'])
+def update_student(id):
+    student = dao.get_student_by_id(id)
+    form = StudentForm(obj=student)
+    return render_template('employee/student_form.html', form=form)
+
+
+@app.route('/employee/students/create', methods=['GET'])
+def create_student():
+    student = Student()
+    form = StudentForm(request.form, obj=student)
+    return render_template('employee/student_form.html', form=form)
+
+
+@app.route('/employee/students/delete/<int:id>')
+def delete_student(id):
+    dao.delete_student(id)
+    return redirect('/employee/students')
+
+
+# ****************
+@app.route('/employee/classrooms/', methods=['GET'])
+def classroom_list():
+    classrooms = dao.get_all_classroom_info()
+    return render_template('/employee/classrooms.html', classrooms=classrooms)
+
+
+@app.route('/employee/classrooms/', methods=['POST'])
+def add_or_update_classroom():
+    form = ClassroomForm()
+    form.students.choices = [(s['id'], s['name']) for s in dao.get_all_student_info()]
+    if form.validate_on_submit():
+        dao.create_or_update_classroom(id=form.id.data, name=form.name.data,
+                                       list_student_id=form.students.data)
+
+        return redirect('/employee/classrooms')
+
+
+@app.route('/employee/classrooms/<int:id>', methods=['GET'])
+def update_classroom(id):
+    classroom = dao.get_classroom_by_id(id)
+    form = ClassroomForm(obj=classroom)
+    form.students.choices = [(s['id'], s['name']) for s in dao.get_all_student_info()]
+    form.students.data = [s.student_id for s in classroom.student_class]
+    return render_template('employee/classroom_form.html', form=form)
+
+
+@app.route('/employee/classrooms/create', methods=['GET'])
+def create_classroom():
+    classroom = Class()
+    form = ClassroomForm(request.form, obj=classroom)
+    return render_template('employee/classroom_form.html', form=form)
+
+
+@app.route('/employee/classrooms/delete/<int:id>')
+def delete_classroom(id):
+    dao.delete_classroom(id)
+    return redirect('/employee/classrooms')
 
 @app.route('/teacher')
 def teacher():
@@ -100,3 +157,4 @@ if __name__ == '__main__':
     with app.app_context():
         dao.init_policy()
         app.run(debug=True)
+        # app.run()
